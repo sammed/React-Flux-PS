dae06da732f39c9fe91cdec7d1c0f95d67c27cba
Samatar Mahamed <samatar.mahamed@softhouse.se>
2018-03-14 15:41:07 +0100

    First module result

--- src/main.js
+++ src/main.js
@@ -1,6 +1,35 @@
+
 $ = jQuery = require('jquery');
+var React = require('react');
+var Home = require('./components/homePage');
+var About = require('./components/about/aboutPage');
 
+(function(win){
+    "use strict";
 
-var App = console.log('Hello world from Browserify');
+    var App = React.createClass({
+        render: function (){
+            var Child;
+    
+            switch(this.props.route) { 
+                case 'about': Child = About; break;
+                default: Child = Home;
+            }
+            return (
+                <div>
+                    <Child/>
+                </div>
+            );
+        }
+    });
+    
+    //Function to render Route depending on the hash change
+    function render (){
+        var route = win.location.hash.substr(1);
+        React.render(<App route={route} />, document.getElementById('app'));
+    }
+    
+    win.addEventListener("hashchange", render);
+    render();
+})(window);
 
-module.exports = App;
